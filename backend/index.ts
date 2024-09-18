a09ade03759fe3e85e584d95a322a9fe2e3f7017
Ashad Malik Kotla <ashadmalikkotla@gmail.com>
2024-09-18 16:42:02 +0500

    setup backend server in TS and refactored folder structure to separate frontend and backend

---
+++ backend/index.ts
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import express, { Application, Request, Response } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
+
+//For env File
+dotenv.config();
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "Hello World" });
+});
+
+const port = process.env.PORT || 3000;
+
+const mongoUrl = process.env.MONGO_URL;
+if (!mongoUrl) {
+  throw new Error("MONGO_URL is not defined in the environment variables");
+}
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    app.listen(port, () => console.log(`Server Port: ${port}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
--- frontend/middleware.cjs
+++ frontend/middleware.cjs
@@ -1,3 +1,4 @@
+//npx json-server --watch db.json --middlewares middleware.cjs --port 3000
 module.exports = (req, res, next) => {
   // res.header("X-Total-Count", "100");
   res.header("Access-Control-Expose-Headers", "X-Total-Count");
